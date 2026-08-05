86826aa3320d0d542b32f9a1f0013382a2ac55e1
jonatastonicioli <tonicioli28@gmail.com>
2026-08-04 22:06:00 -0300

    att

---
+++ CMPLGP2/04.08.2026/Aula_01/Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 2500;
+
+            Console.WriteLine("Número: {0:C}", numero); // saida em moeda
+            Console.WriteLine("Número: {0:D5}", 25); // saida em decimal
+        }
+    }
+}
