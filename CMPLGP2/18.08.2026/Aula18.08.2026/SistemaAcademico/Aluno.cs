746f818893dea1325f54627c251140f6834e9d53
jonatastonicioli <tonicioli28@gmail.com>
2026-08-18 22:09:06 -0300

    att 18.08

---
+++ CMPLGP2/18.08.2026/Aula18.08.2026/SistemaAcademico/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico
+{
+     class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+
+        public double calculaMedia() // nn precisa passar por parametro, esta dentro da classe
+        { // metodo
+            double resultado = (nota1 + nota2) / 2.0;
+
+            return resultado;
+
+        }
+
+    }
+}
---
+++ CMPLGP2/18.08.2026/Aula18.08.2026/SistemaAcademico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+         Aluno qualquernome = new Aluno(); // new cria o objeto
+
+            qualquernome.nome = "Jonatas";
+            qualquernome.nota1 = 7.5;
+            qualquernome.nota2 = 9.2;
+            double rs = qualquernome.calculaMedia();
+
+            Console.WriteLine("Média: {0}", rs);
+
+            
+         //Aluno qualquernome2 = new Aluno(); new cria o objeto
+        }
+    }
+}
