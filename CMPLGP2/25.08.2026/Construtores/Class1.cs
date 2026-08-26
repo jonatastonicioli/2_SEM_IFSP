2368a998c4cd44c363af9a92022699afe67f6e76
jonatastonicioli <tonicioli28@gmail.com>
2026-08-25 22:02:40 -0300

    Construtores,Get,Set,This

---
+++ CMPLGP2/25.08.2026/Construtores/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+     class Aluno
+    {
+        private string nome;
+
+        private double nota1; // nao pode ser acessado usando o operador ponto
+
+        private double nota2;
+
+        public Aluno(string nome) // construtor, inicializa um objeto qnd ele e criado
+        {
+            this.nome = nome; // this, diferencia parametro do metodo
+            nota1 = 0.0;
+            nota2 = 0.0;
+        }
+
+        public Aluno(string nome, double nota1, double nota2) // construtor, inicializa um objeto qnd ele e criado
+        {
+            this.nome = nome; 
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+        public void setNota1(double n1)// valida a nota
+        {
+            if (n1>=0 && n1<=10)
+            {
+                this.nota1 = nota1;
+            }
+        }
+
+        public double getNota1()
+        {
+            return nota1;
+        }
+
+        public void setNota2(double n2)// valida a nota
+        {
+            if (n2 >= 0 && n2 <= 10)
+            {
+                nota2 = n2;
+            }
+        }
+
+        public double getNota2()
+        {
+            return nota2;
+        }
+
+        public void setNotas(double nota1, double nota2)
+        {
+            setNota1(nota1);
+            setNota2(nota2);
+        }
+        public double media ()
+        {
+            double resultado = (nota1 + nota2) / 2.0;
+
+            return resultado;
+        }
+
+    }
+}
---
+++ CMPLGP2/25.08.2026/Construtores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno("Ana", 7.5, 8.9); // objeto passando 3 parametros
+            
+            // Aluno a1 = new Aluno("Ana"); objeto que recebe uma string
+
+
+            //a1.setNota1(10.0);  //como o nota1 esta com o metodo private nao da 
+            //para acessá-lo com o operador ponto, assim é criado um metodo void na classe
+            //para valida-lo
+            //a1.setNota2(5.0);
+
+
+            //a1.nota1 = 7.5;
+            //a1.nota2 = 9.2;
+
+
+            a1.setNotas(8.0, 7.0); // se fazer o metodo setNotas da pra chamar direto
+     
+
+            Console.WriteLine("Aluno: {0}", a1.getNome());
+            Console.WriteLine("Nota1: {0}", a1.getNota1());
+            Console.WriteLine("Nota2: {0}", a1.getNota2());
+            Console.WriteLine("Media: {0}", a1.media());  
+            
+
+        }
+    }
+}
