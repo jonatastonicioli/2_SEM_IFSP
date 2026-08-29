0a400af649d1ebd75378b3484d9663f33f692105
jonatastonicioli <tonicioli28@gmail.com>
2026-08-29 17:49:52 -0300

    ex.aluno

---
+++ CMPLGP2/Lista.01/Exercicio.aluno/Exercício_Tempo/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Tempo
+{
+    internal class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+
+        public double CalculaMedia()
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+    }
+}
---
+++ CMPLGP2/Lista.01/Exercicio.aluno/Exercício_Tempo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Tempo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno ficha = new Aluno(); // criação objeto
+
+            ficha.nome = "Jônatas";
+            ficha.nota1 = 10;
+            ficha.nota2 = 5;
+
+            double resultado = ficha.CalculaMedia();
+
+
+
+
+            Console.WriteLine("O nome do aluno é: {0}", ficha.nome);
+            Console.WriteLine("A média é: {0}", resultado);
+        }
+    }
+}
