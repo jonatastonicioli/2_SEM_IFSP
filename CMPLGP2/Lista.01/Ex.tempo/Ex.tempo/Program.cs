f446ad6efe821d4deb0bc671690838d6bb25c1c6
jonatastonicioli <tonicioli28@gmail.com>
2026-08-30 14:46:52 -0300

    Exercício tempo finalizado

--- CMPLGP2/Lista.01/Ex.tempo/Ex.tempo/Program.cs
+++ CMPLGP2/Lista.01/Ex.tempo/Ex.tempo/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex.tempo
+namespace Tempo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Tempo t0 = new Tempo(); // cai no construtor 1
+
+            t0.setHora(23);
+            t0.setMinuto(30);
+            t0.setSegundo(12);
+
+            t0.imprimeTempoSimples();
+            t0.imprimeTempoExtenso();
+
+            //t0.setHora(25); , da erro pela validação do set na classe
+            //Console.WriteLine(t0.getHora());
+
+            Tempo t1 = new Tempo(12, 30, 25); //cai no construtor 2
+            // assumindo que os valores passados como parametros estão corretos
+            // se os valores fossem incorretos, dava para usar os setter no construtor para validar
+
+
+            t1.setHora(25);
+            t1.setMinuto(25);
+            t1.setSegundo(50);
+
+            t1.imprimeTempoSimples();
+            t1.imprimeTempoExtenso();
 
         }
     }
--- CMPLGP2/Lista.01/Ex.tempo/Ex.tempo/Tempo.cs
+++ CMPLGP2/Lista.01/Ex.tempo/Ex.tempo/Tempo.cs
@@ -1,17 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex.tempo
+namespace Tempo
 {
     internal class Tempo
     {
         private int hora;
         private int minuto;
-        private int segundo;
+        private int segundo; // n pode ser acessado com o operador ponto
 
+        public Tempo() //construtor sem parametros
+        {
+            hora = 23;
+            minuto = 59;
+            segundo = 59;
+        }
 
+        public Tempo(int hora, int minuto, int segundo) // construtor com parametros
+
+        {
+            this.hora = hora; //this.hora é o campo, hora é o parâmetro
+            this.minuto = minuto;
+            this.segundo = segundo;
+        }
+
+        public void setHora(int hora)
+        {
+            if (hora >= 0 && hora <= 23)
+            {
+                this.hora = hora;
+            }
+            else
+            {
+                Console.WriteLine("O valor da hora deve ser entre 0 e 23");
+            }
+        }
+
+        public void setMinuto(int minuto)
+        {
+            if (minuto >= 0 && minuto <= 59)
+            {
+                this.minuto = minuto;
+            }
+            else
+            {
+                Console.WriteLine("O valor do minuto deve ser entre 0 e 59");
+            }
+        }
+
+        public void setSegundo(int segundo)
+        {
+            if (segundo >= 0 && segundo <= 59)
+            {
+                this.segundo = segundo;
+            }
+            else
+            {
+                Console.WriteLine("O valor do segundo dever ser entre 0 e 59");
+            }
+        }
+
+        public int getHora()
+        {
+            return hora;
+        }
+        public int getMinuto()
+        {
+            return minuto;
+        }
+        public int getSegundo()
+        {
+            return segundo;
+        }
+        public int totalSegundos()
+        {
+            int segundos;
+            return segundos = (hora * 3600 + minuto * 60 + segundo);
+        }
+        public void imprimeTempoSimples()
+        {
+            Console.WriteLine("{0:D2}:{1:D2}:{2:D2}", hora, minuto, segundo); //:D2 para sempre formatar com dois dígitos mesmo se for <0
+        }
+        public void imprimeTempoExtenso()
+        {
+            Console.WriteLine("{0} hora(s), {1} minuto(s) e {2} segundo(s)", hora, minuto, segundo);
+        }
+           
     }
 }
+        
+        
+       
+
+
+
